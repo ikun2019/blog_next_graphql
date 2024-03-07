5e5084d487f8ee7f9fcb3474a0b6842d1383d701
ikun <0215.miyatto@gmail.com>
2024-03-08 06:17:56 +0900

    signupメソッドの作成

---
+++ api/src/resolvers/Mutation.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+APP_SECRET = 'graphql';
+
+async function signup(parent, args, context) {
+  const password = await bcrypt.hash(args.password, 10);
+  const user = await context.prisma.user.create({
+    data: {
+      ...args,
+      password,
+    }
+  });
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+  return {
+    token,
+    user,
+  };
+};
+
+module.exports = {
+  signup,
+}
