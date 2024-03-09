b09f3db3d22d36aed91cab54e034da1df1bcd32b
ikun <0215.miyatto@gmail.com>
2024-03-09 10:57:48 +0900

    server.jsにresolverを読み込み

--- api/src/server.js
+++ api/src/server.js
@@ -5,29 +5,21 @@ const path = require('path');
 const { PrismaClient } = require('@prisma/client');
 const { getUserId } = require('./utils');
 
+// resolverの読み込み
+const Query = require('./utils');
+const Mutation = require('./resolvers/Mutation');
+const Link = require('./resolvers/Link');
+const User = require('./resolvers/User');
+
 const prisma = new PrismaClient();
 
 // リゾルバ関数
 // 値を入れる
 const resolvers = {
-  Query: {
-    info: () => "HackerNewsクローン",
-    feed: () => (parent, args, context) => {
-      return context.prisma.link.findMany();
-    },
-  },
-
-  Mutation: {
-    post: (parent, args, context) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        }
-      });
-      return newLink;
-    }
-  },
+  Query,
+  Mutation,
+  Link,
+  User,
 };
 
 const server = new ApolloServer({
